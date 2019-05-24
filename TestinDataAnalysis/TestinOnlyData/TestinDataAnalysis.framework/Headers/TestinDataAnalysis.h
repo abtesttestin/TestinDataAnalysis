b0cd91990a5d5e767cd5fd547b0c9892d7f6c0ca
wubo <yemo321@163.com>
2019-05-24 21:38:50 +0800

    update framework 5.1.5 B

--- TestinDataAnalysis/TestinOnlyData/TestinDataAnalysis.framework/Headers/TestinDataAnalysis.h
+++ TestinDataAnalysis/TestinOnlyData/TestinDataAnalysis.framework/Headers/TestinDataAnalysis.h
@@ -2,7 +2,7 @@
 //  TestinDataAnalysis.h
 //  TestinDataAnalysis SDK version 5.1.5
 //  BugOut version 1.0.7
-//  build:A
+//  build:B
 //  module:Data
 //  Created by Testin on 16/7/29.
 //  Copyright © 2016年 Testin. All rights reserved.
